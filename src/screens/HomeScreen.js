20391352d836f616c203b4ba4ef56aae003bd213
Temilayo Subair <olajnrsubair@gmail.com>
2020-08-19 12:02:05 +0100

    update

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -15,209 +15,209 @@ import { BASE_URL } from '../config';
 import { UserContext } from '../contexts/UserContext';
 
 const Toast = ({ visible, message }) => {
-	if (visible) {
-		ToastAndroid.showWithGravityAndOffset(message, ToastAndroid.LONG, ToastAndroid.CENTER, 25, 50);
-		return null;
-	}
-	return null;
+    if (visible) {
+        ToastAndroid.showWithGravityAndOffset(message, ToastAndroid.LONG, ToastAndroid.CENTER, 25, 50);
+        return null;
+    }
+    return null;
 };
 
 export default function HomeScreen({ navigation }) {
-	const { logout } = React.useContext(AuthContext);
-	const user = React.useContext(UserContext);
+    const { logout } = React.useContext(AuthContext);
+    const user = React.useContext(UserContext);
 
-	const token = user.token;
-	const user_id = user.user_id;
+    const token = user.token;
+    const user_id = user.user_id;
 
-	const initialValue = [];
-	const [location, setLocation] = React.useState(null);
-	const [errorMsg, setErrorMsg] = React.useState(null);
-	const [number, setNumber] = React.useState(initialValue);
-	const [data, setData] = React.useState(initialValue);
-	const [loading, setLoading] = React.useState(true);
-	const [visibleToast, setvisibleToast] = React.useState(false);
+    const initialValue = [];
+    const [location, setLocation] = React.useState(null);
+    const [errorMsg, setErrorMsg] = React.useState(null);
+    const [number, setNumber] = React.useState(initialValue);
+    const [data, setData] = React.useState(initialValue);
+    const [loading, setLoading] = React.useState(true);
+    const [visibleToast, setvisibleToast] = React.useState(false);
 
-	React.useEffect(() => setvisibleToast(false), [visibleToast]);
+    React.useEffect(() => setvisibleToast(false), [visibleToast]);
 
-	const handleButtonPress = () => {
-		setvisibleToast(true);
-	};
+    const handleButtonPress = () => {
+        setvisibleToast(true);
+    };
 
-	React.useEffect(() => {
-		navigation.setOptions({
-			headerRight: () => (
-				<HeaderIconButton
-					name={'log-out'}
-					onPress={() => {
-						logout();
-					}}
-				/>
-			),
-		});
-	}, [navigation]);
+    React.useEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <HeaderIconButton
+                    name={'log-out'}
+                    onPress={() => {
+                        logout();
+                    }}
+                />
+            ),
+        });
+    }, [navigation]);
 
-	React.useEffect(() => {
-		axios
-			.get(`${BASE_URL}/emergency-contacts/${user_id}`, {
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			})
-			.then(({ data }) => {
-				setData(data);
-				setLoading(false);
-			});
-	}, [token, user_id]);
+    React.useEffect(() => {
+        axios
+            .get(`${BASE_URL}/emergency-contacts/${user_id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            .then(({ data }) => {
+                setData(data);
+                setLoading(false);
+            });
+    }, [token, user_id]);
 
-	React.useEffect(() => {
-		(async () => {
-			let { status } = await Location.requestPermissionsAsync();
-			if (status !== 'granted') {
-				setErrorMsg('Permission to access location was denied');
-			}
+    React.useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+            }
 
-			let location = await Location.getCurrentPositionAsync({});
-			setLocation(location);
-			setLoading(false);
-		})();
-	}, []);
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+            setLoading(false);
+        })();
+    }, []);
 
-	let text = 'Waiting..';
-	if (errorMsg) {
-		text = errorMsg;
-	} else if (location) {
-		text = JSON.parse(JSON.stringify(location));
-	}
+    let text = 'Waiting..';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.parse(JSON.stringify(location));
+    }
 
-	function renderNumber() {
-		if (loading) {
-			return <Loading loading={loading} />;
-		} else {
-			const emergencyNumber = {
-				firstNumber: data.data[0],
-				sencondNumber: data.data[1],
-			};
-			AsyncStorage.setItem('emergencyNumber', JSON.stringify(emergencyNumber));
+    function renderNumber() {
+        if (loading) {
+            return <Loading loading={loading} />;
+        } else {
+            const emergencyNumber = {
+                firstNumber: data.data[0],
+                sencondNumber: data.data[1],
+            };
+            AsyncStorage.setItem('emergencyNumber', JSON.stringify(emergencyNumber));
 
-			function sendSmsFunction() {
-				var phoneNumbers = {
-					addressList: [data.data[0], data.data[1]],
-				};
-				var message = `See my location here: http://maps.google.com/maps?q=${text.coords.latitude},${text.coords.longitude} please pay close attention to where i am `;
-				SmsAndroid.send(
-					JSON.stringify(phoneNumbers),
-					message,
-					(fail) => {
-						console.log('Failed with this error: ' + fail);
-					},
-					(success) => {
-						console.log('SMS sent successfully' + success);
-					}
-				);
-			}
-			return (
-				<View style={styles.container}>
-					<Text fontSize="6xl">{data.data[0]}</Text>
+            function sendSmsFunction() {
+                var phoneNumbers = {
+                    addressList: [data.data[0], data.data[1]],
+                };
+                var message = `See my location here: http://maps.google.com/maps?q=${text.coords.latitude},${text.coords.longitude} please pay close attention to where i am `;
+                SmsAndroid.send(
+                    JSON.stringify(phoneNumbers),
+                    message,
+                    (fail) => {
+                        console.log('Failed with this error: ' + fail);
+                    },
+                    (success) => {
+                        console.log('SMS sent successfully' + success);
+                    }
+                );
+            }
+            return (
+                <View style={styles.container}>
+                
+                <Button
+                        onPress={() => {
+                            sendSmsFunction();
+                            handleButtonPress();
+                        }}
+                        mb={200}
+                        bg="brand900"
+                        h={100}
+                        w={200}
+                        mx="xl"
+                        rounded="circle"
+                        shadow="2xl"
+                        // borderWidth={10}
+                        // borderColor="red500"
+                        underlayColor="brand900"
+                    >
+                        SEND SMS
+                    </Button>
+                    <Toast style={styles.toast} visible={visibleToast} message="Message Successfully sent" />
+                </View>
+            );
+        }
+    }
 
-					<Text> {data.data[1]}</Text>
-					<Text>Click on Butonn below in time of emergency</Text>
-					<Button
-						onPress={() => {
-							sendSmsFunction();
-							handleButtonPress();
-						}}
-						bg="red500"
-						h={60}
-						w={60}
-						mx="xl"
-						rounded="circle"
-						shadow="md"
-						borderless
-					>
-						<Icon name="location-pin" size={4} color="white" fontFamily="Entypo" />
-					</Button>
-					<Toast style={styles.toast} visible={visibleToast} message="Message Successfully sent" />
-				</View>
-			);
-		}
-	}
+    React.useEffect(() => {
+        AsyncStorage.getItem('emergencyNumber').then((emergencyNumber) => {
+            setNumber(JSON.parse(emergencyNumber));
+        });
+    }, []);
 
-	React.useEffect(() => {
-		AsyncStorage.getItem('emergencyNumber').then((emergencyNumber) => {
-			setNumber(JSON.parse(emergencyNumber));
-		});
-	}, []);
+    function isNumberSaved() {
+        if (number) {
+            function sendSmsFunction() {
+                var phoneNumbers = {
+                    addressList: [number.firstNumber, number.sencondNumber],
+                };
+                var message = `See my location here: http://maps.google.com/maps?q=${text.coords.latitude},${text.coords.longitude} please pay close attention to where i am `;
+                SmsAndroid.send(
+                    JSON.stringify(phoneNumbers),
+                    message,
+                    (fail) => {
+                        console.log('Failed with this error: ' + fail);
+                    },
+                    (success) => {
+                        console.log('SMS sent successfully' + success);
+                    }
+                );
+            }
 
-	function isNumberSaved() {
-		if (number) {
-			function sendSmsFunction() {
-				var phoneNumbers = {
-					addressList: [number.firstNumber, number.sencondNumber],
-				};
-				var message = `See my location here: http://maps.google.com/maps?q=${text.coords.latitude},${text.coords.longitude} please pay close attention to where i am `;
-				SmsAndroid.send(
-					JSON.stringify(phoneNumbers),
-					message,
-					(fail) => {
-						console.log('Failed with this error: ' + fail);
-					},
-					(success) => {
-						console.log('SMS sent successfully' + success);
-					}
-				);
-			}
+            return (
+                <View style={styles.view}>
+                    
+                    <Button
+                        onPress={() => {
+                            sendSmsFunction();
+                            handleButtonPress();
+                        }}
+                        mb={200}
+                        bg="brand900"
+                        h={100}
+                        w={200}
+                        mx="xl"
+                        rounded="circle"
+                        shadow="2xl"
+                        // borderWidth={10}
+                        // borderColor="red500"
+                        underlayColor="brand900"
+                    >
+                        SEND SMS
+                    </Button>
+                    <Toast style={styles.toast} visible={visibleToast} message="Message Successfully sent" />
+                </View>
+            );
+        } else {
+            return renderNumber();
+        }
+    }
 
-			return (
-				<View style={styles.view}>
-					
-					<Button
-						onPress={() => {
-							sendSmsFunction();
-							handleButtonPress();
-						}}
-						mb={200}
-						bg="brand900"
-						h={130}
-						w={130}
-						mx="xl"
-						rounded="circle"
-						shadow="2xl"
-						borderWidth={10}
-						// borderColor="red500"
-						underlayColor="brand900"
-					>
-						<Icon name="location-pin" color="white" fontFamily="Entypo" />
-					</Button>
-					<Toast style={styles.toast} visible={visibleToast} message="Message Successfully sent" />
-				</View>
-			);
-		} else {
-			return renderNumber();
-		}
-	}
-
-	return <View style={styles.container}>{isNumberSaved()}</View>;
+    return <View style={styles.container}>{isNumberSaved()}</View>;
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	button: {
-		padding: 10,
-		borderWidth: 0.5,
-		borderColor: '#bbb',
-		margin: 10,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	toast: {
-		paddingTop: Constants.statusBarHeight,
-	},
-	view: {
-		paddingLeft: 50,
-		paddingRight: 0
-	},
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        padding: 10,
+        borderWidth: 0.5,
+        borderColor: '#bbb',
+        margin: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    toast: {
+        paddingTop: Constants.statusBarHeight,
+    },
+    // view: {
+    //  paddingLeft: 50,
+    //  paddingRight: 0
+    // },
 });
