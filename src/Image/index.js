17e920b31df06d093c5148d95f34f92238d95eb7
Temilayo Subair <olajnrsubair@gmail.com>
2020-10-19 11:25:37 +0100

    added splash image

---
+++ src/Image/index.js
@@ -0,0 +1,1 @@
+export const SplashImage = require('./splash.png')
--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { SplashImage } from "../Image";
 
 export default function SplashScreen() {
-	return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Image source={SplashImage} style={{width: "100%", height: 250}} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: 'purple',
-	},
+  container: {
+    flex: 1,
+    // backgroundColor: "white",
+    // flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "10%",
+  },
 });
